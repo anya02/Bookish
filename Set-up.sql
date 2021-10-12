cb3ebbb20da7f8bebd2e98fdc0fd1b23dba4530b
Anya Murphy <anya.murphy@mac.com>
2021-10-12 11:33:39 +0100

    Finalised all tables
    Co-authored-by: JabrilMahamud <JabrilMahamud@users.noreply.github.com>

--- Set-up.sql
+++ Set-up.sql
@@ -3,31 +3,27 @@ CREATE TABLE authors (
 	name varchar(50) NOT NULL
 )
 
-CREATE TABLE books(
-	id SERIAL PRIMARY KEY,
-	name varchar(50) NOT NULL,
-	foreign KEY (authors_id) REFERENCES authors(authors_id),
-	isbn BIGINT(9999999999999) NOT NULL,
-	numberOfCopies INT(127) NOT INT
-)
-
-)
-CREATE Checkouts(
-	id SERIAL PRIMARY KEY,
-	due_date Date NOT NULL,
-	
-)
-
--- stuff that needs to get linked--
-
 CREATE TABLE accounts(
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
-	password varchar(20) NOT NULL,
-	username varchar(20) NOT NULL
+	username varchar(20) NOT NULL,
+	password varchar(20) NOT NULL
+)
 
--- linking part--
-SELECT book.name
-from books
-	JOIN authors ON books.authors_id = authors_id
--- foreign key--
+CREATE TABLE books (
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	isbn BIGINT NOT NULL,
+	numberOfCopies INT NOT NULL,
+	authorID INT,
+	foreign KEY (authorId) REFERENCES authors(id)
+)
+
+CREATE TABLE checkouts (
+	id SERIAL PRIMARY KEY,
+	due_date Date NOT NULL,
+	accountID INT,
+	bookID INT,
+	foreign KEY (accountID) REFERENCES accounts(id),
+	foreign KEY (bookID) REFERENCES books(id)
+)
