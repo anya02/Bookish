feb8446fe7bbf3655e087de76fe1b9ca2db0fbc1
Anya Murphy <anya.murphy@mac.com>
2021-10-12 11:47:37 +0100

    Added semicolons

--- Set-up.sql
+++ Set-up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE authors (
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL
-)
+);
 
 CREATE TABLE accounts(
 	id SERIAL PRIMARY KEY,
 	name varchar(50) NOT NULL,
 	username varchar(20) NOT NULL,
 	password varchar(20) NOT NULL
-)
+);
 
 CREATE TABLE books (
 	id SERIAL PRIMARY KEY,
@@ -17,7 +17,7 @@ CREATE TABLE books (
 	numberOfCopies INT NOT NULL,
 	authorID INT,
 	foreign KEY (authorId) REFERENCES authors(id)
-)
+);
 
 CREATE TABLE checkouts (
 	id SERIAL PRIMARY KEY,
@@ -26,4 +26,4 @@ CREATE TABLE checkouts (
 	bookID INT,
 	foreign KEY (accountID) REFERENCES accounts(id),
 	foreign KEY (bookID) REFERENCES books(id)
-)
+);
